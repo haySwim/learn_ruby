14b3b8ac176d930e2cffe96a14d80be790b157b8
Hayley Swimelar <hswimelar@gmail.com>
2015-01-22 08:19:12 -0800

    write and pass extra tests for 02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,11 +1,29 @@
+# Adds two numbers.
 def add(a, b)
   a + b
 end
 
+# Subtracts two numbers.
 def subtract(a, b)
   a - b
 end
 
+# Returns the sum of multiple  numbers.
 def sum(numbers)
   numbers.inject(0, :+)
+end
+
+# Returns the product of any multiple numbers.
+def multiply(*numbers)
+  numbers.inject(1, :*)
+end
+
+# Returns the first argument raised the power of the second argument.
+def power(a, b)
+  a**b
+end
+
+# Returns the factorial of a number.
+def factorial(number)
+  (1..number).inject(1, :*)
 end
