41b97f0c82fa60164d28585a7bee70f5ff2f603c
Hayley Swimelar <hswimelar@gmail.com>
2015-01-22 21:56:28 -0800

    pass final test for 03

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -21,6 +21,17 @@ def first_word(string)
   string.split[0]
 end
 
+# Turns takes string and returns title case version.
 def titleize(string)
+  # "Unimportant" words that should not be capitalized
+  # if they are not the first word of the string.
+  small_words = { ' And' => ' and',
+                  ' The' => ' the',
+                  ' A' => ' a',
+                  ' An' => ' an',
+                  ' Over' => ' over' }
+  # Splits the string into array of words, capitalizes them, and rejoins them.
+  # Then matches small words with leading spaces and uncapitalizes them.
   string.split.each(&:capitalize!).join(' ')
+    .gsub(/ \w+/) { |m| small_words.fetch(m, m) }
 end
